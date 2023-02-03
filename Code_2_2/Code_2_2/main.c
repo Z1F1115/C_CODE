ee29a30e86f39ad520f0c45a71d46a3deba4d117
Z1F <F953364@163.com>
2023-02-03 15:11:47 +0800

    操作符

---
+++ Code_2_2/Code_2_2/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+//Á·Ï°
+//int main()
+//{
+//	int a = 13;
+//	int count = 0;
+//	int b = 1;
+//
+//	while (a)
+//	{
+//		int c = 0;
+//		c = a & b;
+//		if (c == 1)
+//		{
+//			count++;
+//		}
+//		a = a >> 1;
+//	}
+//	printf("%d\n",count);
+//	return 0;
+//}
+
+//Á·Ï°
+//int main()
+//{
+//	int a = 13;
+////	int b = 1 << 4 ;
+////	a = a | b;
+//	a = a | (1 << 4);
+//	printf("%d",a);
+//	return 0;
+//}
+
+//Á·Ï°
+//int main()
+//{
+//	int a = 13;
+//	//int b = 1 << 3;
+//	//a = a ^ b;
+//	a = a ^ (1 << 3);
+//	printf("%d", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 13;
+//	a = a & ~(1 << 3);
+//	printf("%d", a);
+//	return 0;
+//}
+
