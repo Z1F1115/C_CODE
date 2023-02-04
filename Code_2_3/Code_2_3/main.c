941960dec77459579e9415ee78ae815e8214ee8d
Z1F <F953364@163.com>
2023-02-04 15:02:14 +0800

    数组练习

---
+++ Code_2_3/Code_2_3/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+//Êý×é
+//Á·Ï°2
+//void init(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = 0;
+//	}
+//}
+//
+//void print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%2d ",arr[i]);
+//	}
+//	printf("\n");
+//
+//}
+//
+//void reverse(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz / 2; i++)
+//	{
+//		int tmp = arr[i];
+//		arr[i] = arr[sz - 1 - i];
+//		arr[sz - 1 - i] = tmp;
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	reverse(arr, sz);
+//	print(arr, sz);
+//	init(arr,sz);
+//	print(arr,sz);
+//	return 0;
+//}
+
+
+//Á·Ï°3
+//int main()
+//{
+//	int arr1[5] = { 1,3,5,7,9 };
+//	int arr2[5] = { 2,4,6,8,10 };
+//	int sz = sizeof(arr1) / sizeof(arr2[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		int tmp = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = tmp;
+//	}
+//	return 0;
+//}
+
