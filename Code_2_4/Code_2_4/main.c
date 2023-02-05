c8a5ff00722497dd0c8c4e60bd01120d6e43fe0e
Z1F <F953364@163.com>
2023-02-05 20:01:23 +0800

    调试

---
+++ Code_2_4/Code_2_4/main.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	int n = 0;
+//	scanf("%d",&n);
+//	for (i = 1; i<=n;i++)
+//	{
+//		int j = 0;
+//		int ret = 1;
+//		for (j = 1; j<=i;j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n",sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
