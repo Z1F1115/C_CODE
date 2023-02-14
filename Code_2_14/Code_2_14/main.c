6adb31b6b61d392e43398b66aa68ad56dbb773e5
Z1F <F953364@163.com>
2023-02-14 22:08:31 +0800

    qsort

---
+++ Code_2_14/Code_2_14/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+//’‚∏ˆ√∞≈›≈≈–Ú±»ΩœÀ¿∞Â
+//void bubble_sort(int * arr, int sz)
+//{
+//	int i = 0;
+//	for (i=0;i<sz-1;i++)
+//	{
+//		int j = 0;
+//		for (j=0;j<sz-1-i;j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = *(arr + j);
+//				*(arr + j) = *(arr + j + 1);
+//				*(arr + j + 1) = tmp;
+//			}
+//
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int  arr[10] = { 9,8,7,6,50,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//
+//	return 0;
+//}
+
+#include <stdlib.h>
+int cmp_int(const void * e1 ,const void * e2)
+{
+	return (*(int*)e1 - *(int*)e2);//…˝–Ú
+}
+int main()
+{
+	int arr[10] = { 9,8,7,6,50,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz,sizeof(arr[0]), cmp_int);
+
+	return 0;
+}
