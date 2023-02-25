763deb4ea04205b71732d53eadf7f1c768101d25
Z1F <F953364@163.com>
2023-02-25 17:01:28 +0800

    指针练习

---
+++ Code_2_25/Code_2_25/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	int a[3][2] = { (0,1),(2,3),(4,5) };
+//	int* p;
+//	p = a[0];
+//	printf("%d", p[0]);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//
+//	printf("%s\n",**++cpp);
+//	printf("%s\n", *-- * ++cpp + 3);
+//	printf("%s\n", *cpp[-2] + 3);
+//	printf("%s\n", cpp[-1][-1] + 1);
+//
+//	return 0;
+//}
+
+/*
+c[4] =
+cp[4] = { "F" , "P" , "N" , "E"}
+cpp
+
+//上面的答案
+point
+ER
+st
+ew
+
+*/
