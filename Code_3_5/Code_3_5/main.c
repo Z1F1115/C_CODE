ac5f5886b4ea2328f0d02d5eaff7604c90a92c08
Z1F <F953364@163.com>
2023-03-05 14:50:04 +0800

    指针作业6题的方法二

---
+++ Code_3_5/Code_3_5/main.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//编程6题解法二
+//#include <string.h>
+//int string_rotate(char *str1, char *str2) {
+//	if (strlen(str1) != strlen(str2)){
+//		return 0;
+//	}
+//	int len = (int)strlen(str1);
+//	strncat(str1, str1, len);
+//	char * ret = strstr(str1,str2);
+//
+//	return ret != NULL;
+//
+//	//if (ret == NULL) {
+//	//	return 0;
+//	//}
+//	//else {
+//	//	return 1;
+//	//}
+//}
+//
+//
+//int main()
+//{
+//	char s1[20] = "AABCD";
+//	char s2[] = "BCDAA";
+//	int ret = string_rotate(s1, s2);
+//	if (ret){
+//		printf("yes\n");
+//	}
+//	else {
+//		printf("no\n");
+//	}
+//	return 0;
+//}
+
+
+int main()
+{
+
+	return 0;
+}
