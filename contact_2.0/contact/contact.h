f473b57db8fbd7047820b5cd9076c63cd0315c55
Z1F <F953364@163.com>
2023-04-22 14:18:23 +0800

    通讯录--动态内存分配

---
+++ contact_2.0/contact/contact.h
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 1000
+#define MAX_NAME  20
+#define MAX_SEX   10
+#define MAX_TELE  12
+#define MAX_ADDR  30
+
+#define DEFAULT_SZ 3
+#define INC_SZ 2
+
+
+//类型的定义
+typedef struct PeoInfo {
+	char name[MAX_NAME];
+	char sex[MAX_SEX];
+	int age;
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+}PeoInfo;
+
+//通讯录
+typedef struct contact {
+	PeoInfo *data;//指向动态申请的空间，用来存放联系人的信息
+	int sz;//通讯录中当前总共有几个
+	int capacity;//记录当前通讯录的最大容量
+}Contact;
+
+//初始化通讯录
+void InitContact(Contact *pc);
+
+//增加联系人
+void AddContact(Contact *pc);
+
+//打印联系人信息
+void PrintContact(const Contact *pc);
+
+//删除联系人信息
+void DelContact(Contact* pc);
+
+//查找指定联系人
+void SearchContact(Contact* pc);
+
+//修改指定联系人
+void ModifyContact(Contact* pc);
+
+//销毁通讯录
+void DestoryContact(Contact* pc);
---
+++ contact_2.0/contact/test.c
@@ -0,0 +1,80 @@
+
+#include "contact.h"
+
+//通讯录-动态增长
+//1.通讯录初始化后，能放3个人信息
+// 1.2当我们空间的存放满后，增加2个人信息
+//每个人的信息：
+//名字+年龄+性别+电话+地址
+//2.增加人的信息
+//3.删除指定人的信息
+//4.修改指定人的信息
+//5.查找指定人的信息
+//6.排序通讯录信息
+//
+
+void menu() {
+	printf("**************************\n");
+	printf("*** 1.add     2.del    ***\n");
+	printf("*** 3.search  4.modify ***\n");
+	printf("*** 5.sort    6.print  ***\n");
+	printf("*** 0.exit             ***\n");
+	printf("**************************\n");
+}
+
+enum Option {
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODUFY,
+	SORT,
+	PRINT
+};
+
+int main() {
+	int input = 0;
+
+	//创建通讯录
+	Contact con;
+	//初始化通讯录
+	//给data申请一块连续的空间在堆区上
+	//sz = 0
+	//capacity初始化为当前的最大容量
+	InitContact(&con);
+
+	do {
+		menu();
+		printf("请选择:>");
+		scanf("%d",&input);
+		switch (input) {
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MODUFY:
+			ModifyContact(&con);
+			break;
+		case SORT:
+			printf("待添加功能\n");
+			break;
+		case PRINT:
+			PrintContact(&con);
+			break;
+		case EXIT:
+			//销毁通讯录
+			DestoryContact(&con);
+			printf("退出通讯录!\n");
+			break;
+		default:
+			printf("选择错误，请重新选择！\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
